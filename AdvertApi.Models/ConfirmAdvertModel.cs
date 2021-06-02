ee26dff07bc3fff08d3cd745809fa543c8bf222a
Rahul Ghiya <rahul20_01@yahoo.com>
2021-06-02 18:04:03 +0100

    Added new solution WebAdvert.AdvertApi for Advert microservice and storing it to AWS DynamoDB table Signed-off-by: Rahul G <rahul20_01@yahoo.com>

---
+++ AdvertApi.Models/ConfirmAdvertModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdvertApi.Models
+{
+    public enum AdvertStatus
+    {
+        Pending = 1,
+        Active = 2
+    }
+
+    public class ConfirmAdvertModel
+    {
+        public string Id { get; set; }
+        public AdvertStatus  Status { get; set; }
+
+    }
+}
---
+++ AdvertApi/Services/DynamoDBAdvertStorage.cs
@@ -0,0 +1,43 @@
+﻿using AdvertApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+
+namespace AdvertApi.Services
+{
+    public class DynamoDBAdvertStorage : IAdvertStorageService
+    {
+        private readonly IMapper _mapper;
+
+        public DynamoDBAdvertStorage(IMapper mapper)
+        {
+            this._mapper = mapper;
+        }
+        public async Task<string> Add(AdvertModel model)
+        {
+            AdvertDBModel advertDBModel = _mapper.Map<AdvertDBModel>(model);
+            advertDBModel.Id = new Guid().ToString();
+            advertDBModel.CreationDateTime = DateTime.UtcNow;
+            advertDBModel.Status = AdvertStatus.Pending;
+
+            using (var client = new AmazonDynamoDBClient())
+            {
+                using (var contxt = new DynamoDBContext (client))
+                {
+                     await contxt.SaveAsync(advertDBModel);
+                }
+            }
+            return advertDBModel.Id;
+
+        }
+
+        public Task<bool> Confirm(ConfirmAdvertModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
